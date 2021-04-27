ab7656739ee6ee8262c87e83abd26c8c5513db32
candiepih <mutheeal.am@gmail.com>
2021-04-27 14:49:01 +0000

    formatted file according to pep8 style

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{:c}".format(i) if i % 2 == 0 else "{:c}".format(65 + (i - 97)), end="")
+    print("{:c}".format(i) if i % 2 == 0 else
+          "{:c}".format(65 + (i - 97)), end="")
