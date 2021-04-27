53967a886c117eda67f75c5457d894ff395199a3
candiepih <mutheeal.am@gmail.com>
2021-04-27 12:08:13 +0000

    formatted file in accordance to pep8 style

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(1, 100):
-	if int(str(i)[::-1]) < i or int(str(i)[::-1]) == i and i > 9:
-		continue;
-	print("{:02d}".format(i), end=", " if i < 89 else "")
+    if int(str(i)[::-1]) < i or int(str(i)[::-1]) == i and i > 9:
+        continue
+    print("{:02d}".format(i), end=", " if i < 89 else "")
 print()
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for c in str:
-		if ord(c) >= 97 and ord(c) <= 122:
-			print("{:c}".format(65 + (ord(c) - 97)), end="")
-		else:
-			print("{}".format(c), end="")
-	print()
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print("{:c}".format(65 + (ord(c) - 97)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()
