bd6f388606a07de0129ee05715eee6c66d407b35
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-04-27 17:45:27 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print(chr(i) if i % 2 is 0 else chr(65 + (i - 97)), end="")
+    print("{:c}".format(i) if i % 2 == 0 else "{:c}".format(65 + (i - 97)), end="")
