ed7694ed513ce2c8b73d731e3c0ab21f1ad46c65
candiepih <mutheeal.am@gmail.com>
2021-04-27 11:44:45 +0000

    formatted files in accordance to betty linter

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts new node at a particullar position of
+ * the sorted linked list
+ * @head: start of linstint_t linked list
+ * @number: member integer value of our listint_t linked list
+ * Return: address of new node or (NULL)on failure
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp;
+	listint_t *our_head;
+	listint_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+	our_head = *head;
+
+	while (our_head != NULL)
+	{
+		if (our_head->next && our_head->next->n >= number)
+		{
+			tmp = our_head->next;
+			our_head->next = new_node;
+			new_node->next = tmp;
+			return (new_node);
+		}
+		our_head = our_head->next;
+	}
+
+	free(new_node);
+	return (NULL);
+}
