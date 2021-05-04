63fe7c657fb45e68df578872cf2c505441bda54c
candiepih <mutheeal.am@gmail.com>
2021-05-04 12:17:27 +0000

    formatted file in accordance to pep8 linting style

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    s =  list(my_string)
+    s = list(my_string)
     for i, char in enumerate(s):
         if (char == "c" or char == "C"):
             s.pop(i)
