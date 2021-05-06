3ee4303441f0432eeb52dd00887d7eed42cf14b5
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-06 17:38:39 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return a_dictionary
+        return None
     k = list(a_dictionary.keys())
     v = list(a_dictionary.values())
     return k[v.index(max(v))]
