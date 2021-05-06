9393a2b39888d2496c6c76969a587b8bd4df27fc
candiepih <mutheeal.am@gmail.com>
2021-05-06 13:56:55 +0000

    formatted file in accordance to betty linting style

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-    new_matrix = [list(map(lambda x: x ** 2, my_list)) for my_list in new_matrix]
+    new_matrix = [list(map(lambda x: x ** 2, my_list))
+                  for my_list in new_matrix]
     return new_matrix
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 mul_by_2 = lambda x: x * 2
+
+
 def multiply_by_2(a_dictionary):
     new_dict = {k: mul_by_2(v) for k, v in a_dictionary.items()}
     return new_dict
-    
