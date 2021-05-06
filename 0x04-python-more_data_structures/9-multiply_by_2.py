a95fa353e96c2689b20be9a15cdf7abb155e31bf
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-06 17:20:49 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    mul_by_2 = lambda x: x * 2
-    new_dict = {k: mul_by_2(v) for k, v in a_dictionary.items()}
+    new_dict = {k: 2 * v for k, v in a_dictionary.items()}
     return new_dict
