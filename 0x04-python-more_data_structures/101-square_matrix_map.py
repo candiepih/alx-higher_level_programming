976d4556e49ba1d28067d3a03d5a3136a95943c5
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-07 14:16:34 +0300

    Update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    newlist = list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
-    return newlist
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
