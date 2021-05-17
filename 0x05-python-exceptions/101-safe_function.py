fb11f9ef48cbca86142ca3f10e00641e982c6991
candiepih <mutheeal.am@gmail.com>
2021-05-17 11:32:47 +0000

    added new advanced task files

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as exception:
+        print("Exception: {}".format(exception), file=stderr)
+        return None
