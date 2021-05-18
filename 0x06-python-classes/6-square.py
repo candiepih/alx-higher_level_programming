e15b89339f8265e8fbb620ac8bc9a7fd35bda924
candiepih <mutheeal.am@gmail.com>
2021-05-18 13:19:50 +0000

    added new task files

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Derives a square """
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data
+        Args:
+            size (int): size of the square
+            position (tuple): two positive integers
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+        Raises:
+            TypeError: when `size` isn't an integer
+            ValueError: `size` is less than 0
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """Calculates the area of a square
+        Returns: the area of the square
+        """
+
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Retrieves the value of `size`"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of `size`
+        Args:
+            value (int): value to be set to `size`
+        Raise:
+            TypeError: when `value` isn't an integer
+            ValueError: `value` is less than 0
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        """Prints a square to stdout using #"""
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            for i in range(self.__position[0]):
+                if self.__position[1] > 0:
+                    print("_", end="")
+                else:
+                    print(" ", end="")
+            for i in range(self.__size):
+                print("#", end="")
+            print()
+
+    @property
+    def position(self):
+        """Retrieves `position` value """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the value of `position`
+        Args:
+            value (int): value to be set to `position` attribute
+        Raise:
+            TypeError: position isn't a tupple or doesn't contain 2
+                       elements or has negative integers
+        """
+
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2 or (
+                           (not isinstance(v, int) or v < 0) for v in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
