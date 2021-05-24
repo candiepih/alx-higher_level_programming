95dabed7b79f48853a0e295ea63a60747228091b
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-24 23:13:24 +0300

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -54,4 +54,6 @@ class Rectangle:
 
     def perimeter(self):
         """Calculates the perimeter of a reactangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return ((self.__width + self.__height) * 2)
