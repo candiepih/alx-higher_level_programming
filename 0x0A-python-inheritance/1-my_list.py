40d418e2d1814084fb258860bf82689aba7b82e4
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-05-31 18:36:13 +0300

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """Class that extends the list base class"""
+    def __init__(self):
+        """Initialize inherited data"""
+        self.__init__()
+
     def print_sorted(self):
         """
             Prints a sorted list
