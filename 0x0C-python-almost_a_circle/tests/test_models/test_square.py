9d9c664ee3d59314ed53d9474e1f4bfca8c9b30c
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-06-08 17:18:12 +0300

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,5 +1,6 @@
 """Contains unittest cases for `Square` class"""
 import unittest
+from models.base import Base
 from models.square import Square
 
 
@@ -7,4 +8,21 @@ class TestSquare(unittest.TestCases):
     """Tests all methods and attributes of the `Square`
     class
     """
-    
+    def test_size(self):
+        """Test creating `Square` with integer size"""
+        Base._Base__nb_objects = 0
+        s1 = Square(5)
+        self.assertEqual(s1.size, 5)
+
+    def test_size_with_string(self):
+        """Test size with string"""
+        try:
+            s1 = Square("5k")
+        except TypeError as e:
+            self.assertEqual(e, "width must be an integer")
+
+    def test_printing_square(self):
+        """Test string output of `Square`"""
+        Base._Base__nb_objects = 0
+        s1 = Square(5)
+        self.assertEqual(s1.__init__(), "[Square] (1) 0/0 - 5")
