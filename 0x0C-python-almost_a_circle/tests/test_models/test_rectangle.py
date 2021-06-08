2e98a9279a398c5adf5aa5553555e7959cf3ac22
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-06-08 16:44:23 +0300

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -214,17 +214,78 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             r3.y = "5"
 
-    # Test for area
     def test_for_area(self):
         """Testing for area"""
         Base._Base__nb_objects = 0
         r1 = Rectangle(3, 2)
         self.assertEqual(r1.area(), 6)
 
-    # Test for display
-    def test_for_display(self):
-        """Test for displaying `Rectangle`"""
+    def test_for_str_with_id(self):
+        """Test formatted output class to string with id"""
         Base._Base__nb_objects = 0
-        r1 = Rectangle(6, 1)
-        self.assertEqual(r1.display(), None)
-        
+        r1 = Rectangle(4, 6, 2, 1, 12)
+        self.assertEqual(r1.__str__(), "[Rectangle] (12) 2/1 - 4/6")
+
+    def test_for_str_without_id(self):
+        """Test formatted output class to string without id"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(4, 6, 2, 1, 12)
+        self.assertEqual(r1.__str__(), "[Rectangle] (1) 2/1 - 4/6")
+
+    def test_updating_id(self):
+        """Testing for updating id"""
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 10/10")
+
+    def test_updating_width(self):
+        """Testing for updating width"""
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 2)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 2/10")
+
+    def test_updating_height(self):
+        """Testing for updating height"""
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 2, 3)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 10/10 - 2/3")
+
+    def test_updating_x(self):
+        """Testing for updating x"""
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/10 - 2/3")
+
+    def test_updating_y(self):
+        """Testing for updating y"""
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
+
+    def test_kwarg_update_1(self):
+        """Testing  updating width and x with kwarg"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(width=1, x=2)
+        self.assertEqual(r1.__str__(), "[Rectangle] (1) 2/10 - 1/10")
+
+    def test_kwarg_update_2(self):
+        """Testing updating y, x, width, id with kwarg"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(r1.__str__(), "[Rectangle] (89) 3/1 - 2/10")
+
+    def test_kwarg_update_3(self):
+        """Testing updating x, height, y and width with kwarg"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(r1.__str__(), "[Rectangle] (1) 1/3 - 4/2")
+
+    def test_kwarg_update_4(self):
+        """Testing updating height with kwarg"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(height=1)
+        self.assertEqual(r1.__str__(), "[Rectangle] (1) 10/10 - 10/1")
