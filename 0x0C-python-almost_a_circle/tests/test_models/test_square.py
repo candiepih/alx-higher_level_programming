2f9e9aafd26052795612920b211ad19ace566eb9
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-06-08 18:01:01 +0300

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -14,10 +14,46 @@ class TestSquare(unittest.TestCase):
         s1 = Square(5)
         self.assertEqual(s1.size, 5)
 
-    def test_size_with_string(self):
+    def test_size_string(self):
         """Test size with string"""
         with self.assertRaises(TypeError) as e:
             s1 = Square("5k")
+            self.assertEqual(e, "width must be an integer")
+
+    def test_size_list(self):
+        """Test size with list"""
+        with self.assertRaises(TypeError) as e:
+            s1 = Square([3, 5, 2, 4])
+            self.assertEqual(e, "width must be an integer")
+
+    def test_size_dict(self):
+        """Test size with dictionary"""
+        with self.assertRaises(TypeError) as e:
+            s1 = Square({"n": 2, "b": 4})
+            self.assertEqual(e, "width must be an integer")
+
+    def test_size_tuple(self):
+        """Test size with tuple"""
+        with self.assertRaises(TypeError) as e:
+            s1 = Square((3, 5, 2, 4))
+            self.assertEqual(e, "width must be an integer")
+
+    def test_size_float(self):
+        """Test size with float"""
+        with self.assertRaises(TypeError) as e:
+            s1 = Square(6.96)
+            self.assertEqual(e, "width must be an integer")
+
+    def test_size_nan(self):
+        """Test size with not a number(nan)"""
+        with self.assertRaises(TypeError) as e:
+            s1 = Square(float('nan'))
+            self.assertEqual(e, "width must be an integer")
+
+    def test_size_inf(self):
+        """Test size with Infinity(inf)"""
+        with self.assertRaises(TypeError) as e:
+            s1 = Square(float('inf'))
             self.assertEqual(e, "width must be an integer")
 
     def test_printing_square(self):
