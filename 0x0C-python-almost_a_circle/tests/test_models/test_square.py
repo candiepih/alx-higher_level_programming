190a32c3f0538316e05191692390ec8a1c465368
candiepih <mutheeal.am@gmail.com>
2021-06-08 14:04:20 +0000

    added new test file

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,1 +1,10 @@
+"""Contains unittest cases for `Square` class"""
+import unittest
+from models.square import Square
 
+
+class TestSquare(unittest.TestCases):
+    """Tests all methods and attributes of the `Square`
+    class
+    """
+    
