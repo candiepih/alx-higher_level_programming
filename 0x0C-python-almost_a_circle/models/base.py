2f76a24ea1f8ed752c32cd455c18d31d0f855c23
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-06-09 09:19:23 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -68,7 +68,7 @@ class Base:
         Returns:
             returns an instance with all attributes already set
         """
-        dummy = cls(1, 1, 1)
+        dummy = cls(1, 1)
         dummy.update(**dictionary)
         return dummy
 
