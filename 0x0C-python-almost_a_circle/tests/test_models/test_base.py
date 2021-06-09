2a69e94f6fb1aa4b607c76c9b8599da029805a30
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-06-09 12:50:31 +0300

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -117,8 +117,7 @@ class TestBase(unittest.TestCase):
         Square.save_to_file(None)
         with open("Square.json", "r") as file:
             read = file.read()
-            my_list = Base.from_json_string(read)
-            self.assertListEqual(my_list, [])
+            self.assertEqual(read, [])
 
     def test_save_to_file_Square_empty_list(self):
         """Test saving to file with an empty list"""
