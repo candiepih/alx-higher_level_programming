03f79273ace40031dd48994b3f512723f333dd32
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-07-12 18:31:07 +0300

    Update 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,4 @@
 -- Imports in `hbtn_0c_0` database table dump from file
--- and finds
-SET autocommit=0; source temperatures.sql; COMMIT;
+-- and finds maximum average temparature depending on state
 SELECT state, MAX(value) AS max_temp FROM `temperatures`
 GROUP BY state ORDER BY state;
