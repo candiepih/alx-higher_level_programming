00fe757a36cb12570514d5d25a34818ef1f5e956
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-07-12 18:28:04 +0300

    Update 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Imports in `hbtn_0c_0` database table dump from file
 -- and finds average temparature
-SET autocommit=1; source temperatures.sql; COMMIT;
+source temperatures.sql; COMMIT;
 SELECT city, AVG(value) AS avg_temp FROM `temperatures`
 GROUP BY city ORDER BY avg_temp DESC;
