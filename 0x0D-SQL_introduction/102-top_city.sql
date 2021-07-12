2e647e751c019c9443b60ba01b16ce7dfe55e4b7
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-07-12 18:30:14 +0300

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,5 @@
 -- Imports in `hbtn_0c_0` database table dump from file
--- and finds
-SET autocommit=0; source temperatures.sql; COMMIT;
+-- and finds first 3 cities with average depending on month 
 SELECT city, AVG(value) AS avg_temp FROM `temperatures`
 WHERE month BETWEEN 7 AND 8
 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
