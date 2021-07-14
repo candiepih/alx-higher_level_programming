ccdc4f22c3e5d69315008f1ef4e2a7af1f978b77
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-07-14 13:06:58 +0300

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,6 @@
 -- script that lists all Comedy shows in the database `hbtn_0d_tvshows`
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id=tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC;
+SELECT tv_shows.title AS title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY title ASC;
