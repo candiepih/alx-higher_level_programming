513db084798612906be106e39859c4af978f1758
candiepih <mutheeal.am@gmail.com>
2021-08-02 12:29:49 +0000

    initial commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+if (argv.length <= 2 || argv.length === 3) {
+  console.log(0);
+} else {
+  const newArgv = argv.slice(2, argv.length);
+  newArgv.sort();
+  newArgv.reverse();
+  console.log(newArgv[1]);
+}
