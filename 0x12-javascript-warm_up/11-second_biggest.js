1b94af5643482ba3ec5debf3c35e8ad75c8b3878
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-02 16:27:35 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const newArgv = argv.slice(2, argv.length);
 
-if (argv.length === 2 || argv.length === 3) {
+if (argv.length <= 3) {
   console.log(0);
 } else {
   const { argv } = require('process');
