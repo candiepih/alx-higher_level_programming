52482b0107c4cf941bc3b0549c0f10cfa159f792
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-02 16:30:51 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,7 @@ if (argv.length <= 3) {
   console.log(0);
 } else {
   const newArgv = argv.slice(2, argv.length);
+  newArgv.map((value) => parseInt(value));
   newArgv.sort();
   newArgv.reverse();
   console.log(newArgv[1]);
