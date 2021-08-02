4bb933a6eb12bc6de8f2616eef4d16ffa97bc8ea
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-02 16:36:51 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,8 @@ const { argv } = require('process');
 if (argv.length <= 3) {
   console.log(0);
 } else {
-  const newArgv = argv.slice(2, argv.length);
-  newArgv.map((value) => parseInt(value));
+  let newArgv = argv.slice(2, argv.length);
+  newArgv = newArgv.map((value) => parseInt(value));
   newArgv.sort();
   newArgv.reverse();
   console.log(newArgv[1]);
