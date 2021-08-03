afc8a7004809ebae080a137b0d0f302e1bd82fb2
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-03 21:30:56 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,12 +8,12 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c !== undefined) {
+    if (c === undefined) {
+      this.print();
+    } else {
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
       }
-    } else {
-      this.print();
     }
   }
 }
