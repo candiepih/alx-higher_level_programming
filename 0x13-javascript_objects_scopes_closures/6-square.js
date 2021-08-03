016feb1cafe326404f6dd2afafb0923aa6e11488
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-03 21:40:15 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square1 = require('./5-square.js');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Square1 {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 }
