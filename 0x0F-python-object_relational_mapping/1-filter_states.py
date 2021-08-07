571a3919e82a92e41ad20b36f4ff302217f63a8c
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-07 10:48:24 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,18 +5,19 @@
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-db_name = argv[3]
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                       passwd=password, db=db_name)
-cur = conn.cursor()
-cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id """)
-rows = cur.fetchall()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db_name)
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id """)
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
 
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
