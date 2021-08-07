21dc89f354f7080551be85ef4aced18dc11cd944
candiepih <mutheeal.am@gmail.com>
2021-08-07 07:42:40 +0000

    formatted file to betty linting style

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that lists all cities from the database `hbtn_0e_4_usa`"""
+import MySQLdb
+from sys import argv
+
+username = argv[1]
+password = argv[2]
+db_name = argv[3]
+
+
+conn = MySQLdb.connect(host="localhost", port=3306,  user=username,
+                       passwd=password, db=db_name,  charset="utf8")
+cur = conn.cursor()
+sql = """SELECT cities.id, cities.name, states.name FROM cities
+         INNER JOIN states ON cities.state_id = states.id ORDER
+         BY cities.id ASC"""
+cur.execute(sql)
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""takes in the name of a state as an argument and lists all `cities`
+    of that state, using the database `hbtn_0e_4_usa`
+"""
+
+import MySQLdb
+from sys import argv
+
+username = argv[1]
+password = argv[2]
+db_name = argv[3]
+s_name = argv[4]
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                       passwd=password, db=db_name, charset="utf8")
+cur = conn.cursor()
+sql = """SELECT cities.name FROM cities INNER JOIN states
+         ON cities.state_id = states.id WHERE states.name = %s
+         ORDER BY cities.id ASC"""
+cur.execute(sql, (s_name, ))
+rows = cur.fetchall()
+print(", ".join([row[0] for row in rows]))
+cur.close()
+conn.close()
