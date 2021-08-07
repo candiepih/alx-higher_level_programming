9ec971b286ba9fd6e53638ef3c2006de3659dd3f
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-07 10:50:10 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,19 +6,20 @@
 import MySQLdb
 from sys import argv
 
-username = argv[1]
-password = argv[2]
-db_name = argv[3]
-s_name = argv[4]
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    s_name = argv[4]
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                       passwd=password, db=db_name, charset="utf8")
-cur = conn.cursor()
-sql = """SELECT cities.name FROM cities INNER JOIN states
-         ON cities.state_id = states.id WHERE states.name = %s
-         ORDER BY cities.id ASC"""
-cur.execute(sql, (s_name, ))
-rows = cur.fetchall()
-print(", ".join([row[0] for row in rows]))
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    sql = """SELECT cities.name FROM cities INNER JOIN states
+             ON cities.state_id = states.id WHERE states.name = %s
+             ORDER BY cities.id ASC"""
+    cur.execute(sql, (s_name, ))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
+    conn.close()
