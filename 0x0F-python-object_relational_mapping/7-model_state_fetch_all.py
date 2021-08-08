4aadac5439dc52972b580e3f98c8f4eb0e0df1bf
candiepih <mutheeal.am@gmail.com>
2021-08-08 04:03:15 +0000

    added new task files

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists all `State` objects from the database `hbtn_0e_6_usa`"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__": 
+    Engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(argv[1], argv[2],
+                            argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(Engine)
+    Session = sessionmaker(bind=Engine)
+    session = Session()
+    q = session.query(State).all()
+
+    for item in q:
+        print("{}: {}".format(item.id, item.name))
