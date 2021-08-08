2cde059295763269643fc58231ed334bf47bbad6
candiepih <mutheeal.am@gmail.com>
2021-08-08 06:57:40 +0000

    formatted file to pep8 linting style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=db_name)
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id""")
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
+                ORDER BY id ASC""")
     rows = cur.fetchall()
 
     for row in rows:
