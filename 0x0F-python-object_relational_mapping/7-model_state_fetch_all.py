8e16f63ffce826638bc5ac2e78db670274a4d5ed
candiepih <mutheeal.am@gmail.com>
2021-08-08 04:27:06 +0000

    formatted files in accordance to pep8 style

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,10 +6,10 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     Engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(argv[1], argv[2],
-                            argv[3]), pool_pre_ping=True)
+                           .format(argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(Engine)
     Session = sessionmaker(bind=Engine)
     session = Session()
---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" script that prints the first `State` object from the
+    database `hbtn_0e_6_usa`
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    Engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password,
+                                   db_name), pool_pre_ping=True)
+    Base.metadata.create_all(Engine)
+    Session = sessionmaker(bind=Engine)
+    session = Session()
+    q = session.query(State).order_by(State.id).first()
+    if q is not None:
+        print("{}: {}".format(q.id, q.name))
