a7c7d1cab7596dc9d7b8ba08157c36c23af83122
candiepih <mutheeal.am@gmail.com>
2021-08-08 07:09:40 +0000

    formatted file to pep8 linting style

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,21 +4,19 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-  username = argv[1]
-  password = argv[2]
-  db_name = argv[3]
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
 
+    conn = MySQLdb.connect(host="localhost", port=3306,  user=username,
+                           passwd=password, db=db_name,  charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                INNER JOIN states ON cities.state_id = states.id ORDER
+                BY cities.id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-  conn = MySQLdb.connect(host="localhost", port=3306,  user=username,
-                         passwd=password, db=db_name,  charset="utf8")
-  cur = conn.cursor()
-  cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
-              INNER JOIN states ON cities.state_id = states.id ORDER
-              BY cities.id ASC""")
-  rows = cur.fetchall()
-
-  for row in rows:
-      print(row)
-
-  cur.close()
-  conn.close()
+    cur.close()
+    conn.close()
