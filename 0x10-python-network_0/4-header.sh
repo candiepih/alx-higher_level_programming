c38aa2104a445a0239930496bc4e0a56df53e3cf
candiepih <mutheeal.am@gmail.com>
2021-08-19 12:16:21 +0000

    formatted file to shellcheck linting

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET
-# request to the URL, and displays the body of the response
-# header variable X-HolbertonSchool-User-Id must be sent with the value 98
-
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -s -H "X-HolbertonSchool-User-Id:98" -X GET "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a POST request to the
-# passed URL, and displays the body of the response
-
-email_value='hr@holbertonschool.com'
-subject_value='I will always be here for PLD'
-
-curl -s -d "email=$email_value&subject=$subject_value" -H "Content-Type: application/x-www-form-urlencoded" -X POST "$1"
+# script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -H "Content-Type: application/x-www-form-urlencoded" -X POST "$1"
