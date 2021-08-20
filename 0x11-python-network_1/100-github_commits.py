5d43c4d4b1a0618b3aeaa627fdbf7d1be7ae7ac0
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-08-20 19:38:43 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" script that takes 2 arguments in order to solve this challenge."""
-
+"""script that takes 2 arguments in order to solve this challenge"""
 import requests
 from sys import argv
 
