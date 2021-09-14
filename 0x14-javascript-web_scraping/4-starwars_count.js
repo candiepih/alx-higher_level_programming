6e6923e240b5acc9f4918d8ef3bec48043c7f74a
candiepih <mutheeal.am@gmail.com>
2021-09-14 13:27:38 +0000

    initial commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * Script that prints the number of movies where
+ * the character Wedge Antilles is present..
+ */
+
+const request = require('request');
+const process = require('process');
+const apiUrl = process.argv[2];
+const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+
+request.get(apiUrl, (err, response, body) => {
+  if (err === null) {
+    const data = JSON.parse(body);
+    let films = data.results;
+    films = films.filter(film => film.characters.includes(characterUrl));
+    console.log(films.length);
+  } else {
+    console.log(err);
+  }
+});
